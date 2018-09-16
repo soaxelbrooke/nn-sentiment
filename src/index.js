eea401b8bb930fb7f8c778c612509d32f8230e24
Stuart Axelbrooke <stuart@axelbrooke.com>
2018-09-16 10:32:31 -0700

    Add vectorization

--- src/index.js
+++ src/index.js
@@ -1,24 +1,45 @@
 const tf = require("@tensorflow/tfjs");
 
+const seqLen = 100;
 
 const tokenizeRegexp = new RegExp(/[\w']+|[,.?;\-()]/g);
 const sentTokenizeRegexp = new RegExp(/(?=[.?!])/g);
 
-const sentTokenize = (text) => text.split(sentTokenizeRegexp).reduce((agg, item) => {
-    if (agg.length === 0) {
+const sentTokenize = text =>
+  text
+    .split(sentTokenizeRegexp)
+    .reduce((agg, item) => {
+      if (agg.length === 0) {
         agg.push(item);
-    } else {
+      } else {
         if (item.length > 0) {
-            agg[agg.length - 1] += item[0];
-            agg.push(item.slice(1));
+          agg[agg.length - 1] += item[0];
+          agg.push(item.slice(1));
         }
-    }
-    return agg;
-}, []).filter(chunk => /\w/.test(chunk));
+      }
+      return agg;
+    }, [])
+    .filter(chunk => /\w/.test(chunk));
 
-const tokenize = (text) => (text.toLowerCase().match(tokenizeRegexp) || []);
+const tokenize = text => text.toLowerCase().match(tokenizeRegexp) || [];
+
+const vectorizeTokens = (wordIndexes, tokens) => {
+  const wordIdxs = tokens.map(tok => wordIndexes[tok] || 0).slice(0, seqLen);
+  return tf.tensor([wordIdxs.concat(Array.from({ length: seqLen - wordIdxs.length }, () => 0))]);
+};
+
+const vectorizeBatch = (wordIndexes, batch) => {
+  return tf.tensor(
+    batch.map(tokens => {
+      const wordIdxs = tokens.map(tok => wordIndexes[tok] || 0).slice(0, seqLen);
+      return wordIdxs.concat(Array.from({ length: seqLen - wordIdxs.length }, () => 0));
+    })
+  );
+};
 
 module.exports = {
-    tokenize,
-    sentTokenize
+  tokenize,
+  sentTokenize,
+  vectorizeTokens,
+  vectorizeBatch,
 };
--- test/tokenize.test.js
+++ test/tokenize.test.js
@@ -1,28 +1,49 @@
+const { tokenize, sentTokenize, vectorizeTokens } = require("../src/index");
 
-const {tokenize, sentTokenize} = require('../src/index');
+describe("tokenize", () => {
+  test("splits and lowercases", () => {
+    const tokenized = tokenize("This is how we do it.");
+    expect(tokenized[0]).toBe("this");
+    expect(tokenized[tokenized.length - 1]).toBe(".");
+    expect(tokenized.length).toEqual(7);
+  });
 
-describe('tokenize', () => {
-    test('splits and lowercases', () => {
-        const tokenized = tokenize('This is how we do it.');
-        expect(tokenized[0]).toBe('this');
-        expect(tokenized[tokenized.length - 1]).toBe('.');
-        expect(tokenized.length).toEqual(7);
-    });
+  test("splits sentences", () => {
+    const sentTokenized = sentTokenize(
+      "This is how we do it. It's Friday night, and I feel" +
+        " all right, and the party's here on the west side. So I reach for my 40 and I turn it " +
+        "up. Designated driver take the keys to my truck."
+    );
+    expect(sentTokenized.length).toEqual(4);
+  });
 
-    test('splits sentences', () => {
-        const sentTokenized = sentTokenize("This is how we do it. It's Friday night, and I feel" +
-            " all right, and the party's here on the west side. So I reach for my 40 and I turn it " +
-            "up. Designated driver take the keys to my truck.");
-        expect(sentTokenized.length).toEqual(4);
-    });
+  test("empty texts", () => {
+    const tokenized = tokenize("");
+    expect(Array.isArray(tokenized)).toBe(true);
+    expect(tokenized.length).toEqual(0);
 
-    test('empty texts', () => {
-        const tokenized = tokenize('');
-        expect(Array.isArray(tokenized)).toBe(true);
-        expect(tokenized.length).toEqual(0);
-        
-        const sentTokenized = sentTokenize('');
-        expect(Array.isArray(sentTokenized)).toBe(true);
-        expect(sentTokenized.length).toEqual(0);
-    });
+    const sentTokenized = sentTokenize("");
+    expect(Array.isArray(sentTokenized)).toBe(true);
+    expect(sentTokenized.length).toEqual(0);
+  });
+});
+
+describe("vectorize", () => {
+  const wordIndexes = {
+    this: 1,
+    is: 2,
+    how: 3,
+    do: 4,
+    it: 5,
+    ".": 6,
+  };
+
+  test("turns tokenized text into word indexes", () => {
+    const tokens = ["this", "is", "how", "we", "do", "it", "."];
+    const vectorized = vectorizeTokens(wordIndexes, tokens).dataSync();
+    expect(vectorized[0]).toEqual(wordIndexes["this"]);
+    expect(vectorized[3]).toEqual(0);
+    expect(vectorized[6]).toEqual(6);
+    expect(vectorized.length).toEqual(100);
+  });
 });
