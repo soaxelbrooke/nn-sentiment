152fef4d7781c3088b0c1ea5a4649042bc7b9782
Stuart Axelbrooke <stuart@axelbrooke.com>
2018-09-16 12:58:04 -0700

    Add model, implement prediction

--- Makefile
+++ Makefile
@@ -3,3 +3,8 @@
 fmt:
 	./node_modules/prettier/bin-prettier.js --write 'src/**/*.js'
 	./node_modules/prettier/bin-prettier.js --write 'test/**/*.js'
+
+
+.PHONY: test-server
+test-server:
+	python -m http.server 39283
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const seqLen = 100;
 
 const tokenizeRegexp = new RegExp(/[\w']+|[,.?;\-()]/g);
 const sentTokenizeRegexp = new RegExp(/(?=[.?!])/g);
+let model = null;
+let wordIndexes = null;
 
 const sentTokenize = text =>
   text
@@ -28,6 +30,9 @@ const vectorizeTokens = (wordIndexes, tokens) => {
   return tf.tensor([wordIdxs.concat(Array.from({ length: seqLen - wordIdxs.length }, () => 0))]);
 };
 
+const batchArray = (array, batchSize) =>
+  array.reduce((a, b, i, g) => (!(i % batchSize) ? a.concat([g.slice(i, i + batchSize)]) : a), []);
+
 const vectorizeBatch = (wordIndexes, batch) => {
   return tf.tensor(
     batch.map(tokens => {
@@ -37,9 +42,78 @@ const vectorizeBatch = (wordIndexes, batch) => {
   );
 };
 
+const getModel = modelUrl => {
+  if (model === null) {
+    return tf.loadModel(modelUrl).then(m => {
+      model = m;
+      return model;
+    });
+  } else {
+    return Promise.resolve(model);
+  }
+};
+
+const getWordIndexes = wordIndexesUrl => {
+  if (wordIndexes === null) {
+    return fetch(wordIndexesUrl)
+      .then(r => r.json())
+      .then(wi => {
+        wordIndexes = wi;
+        return wordIndexes;
+      });
+  } else {
+    return Promise.resolve(wordIndexes);
+  }
+};
+
+const predict = (modelBasePath, texts) => {
+  return predictProba(modelBasePath, texts).then(probas =>
+    probas.map(prediction => {
+      if (prediction.detractor > 0.4) {
+        prediction.detractor = 1;
+        prediction.neutral = 0;
+        prediction.promoter = 0;
+      } else if (prediction.promoter > 0.8) {
+        prediction.detractor = 0;
+        prediction.neutral = 0;
+        prediction.promoter = 1;
+      } else {
+        prediction.detractor = 0;
+        prediction.neutral = 1;
+        prediction.promoter = 0;
+      }
+      return prediction;
+    })
+  );
+};
+
+const predictProba = (modelBasePath, texts) => {
+  return Promise.all([
+    getModel(modelBasePath + "/model.json"),
+    getWordIndexes(modelBasePath + "/word_index.json"),
+  ]).then(([model, wordIndexes]) =>
+    model
+      .predict(vectorizeBatch(wordIndexes, texts.map(tokenize)))
+      .data()
+      .then(predictions => batchArray(predictions, 3))
+      .then(probas =>
+        probas.map((proba, idx) => ({
+          text: texts[idx],
+          promoter: proba[2],
+          neutral: proba[1],
+          detractor: proba[0],
+        }))
+      )
+  );
+};
+
 module.exports = {
   tokenize,
   sentTokenize,
   vectorizeTokens,
   vectorizeBatch,
+  predict,
+  predictProba,
+  getModel,
+  getWordIndexes,
 };
